e3e109f0724fffbea4dc08511e9892e73daa494a
anandyadav14012003 <anandyadav14012003@gmail.com>
2026-09-22 17:45:18 +0530

    Update food_records.js

--- food_records.js
+++ food_records.js
@@ -189,7 +189,7 @@ function initLogoutHandler() {
                 if (supabaseClient) {
                     await supabaseClient.auth.signOut();
                 }
-                window.location.href = 'login.html';
+                window.location.href = 'index.html';
             }
         });
     }
